b6ff3184f6e9467f48297aff131db327d9d557fe
Vladimir Goshko <vladimir.goshko@alt-soft.com>
2022-12-04 07:12:27 +0400

    finally almost everything works

---
+++ src/tools.js
@@ -0,0 +1,21 @@
+var path = require('path');
+var url = require('url');
+var fs = require('fs');
+
+module.exports = {
+    GetData : function(usr) {
+        const dataFilePath = path.join(__dirname, "public", "data", usr.toLowerCase() + ".txt");
+        if (fs.existsSync(dataFilePath))
+            return dataFilePath;
+        
+        return "";
+    },
+
+    FullUrl : function(req) {
+        return url.format({
+          protocol: req.protocol,
+          host: req.get('host'),
+          pathname: req.originalUrl
+        });
+    }
+}
